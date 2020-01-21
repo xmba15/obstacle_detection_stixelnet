d57556b3b160d2d7cc09a4f09ec1b2496560cf09
xmba15 <thba1590@gmail.com>
2020-01-22 02:31:52 +0900

    update download script

--- data_loader/kitti_stixel_dataset.py
+++ data_loader/kitti_stixel_dataset.py
@@ -6,21 +6,23 @@ import numpy as np
 
 
 class KittiStixelDataset(object):
-    def __init__(self, data_path, ground_truth_path):
+    def __init__(self, data_path, ground_truth_path, phase="train", batch_size=32, transform=None):
         assert os.path.isdir(data_path)
         assert os.path.isfile(ground_truth_path)
 
-        self.__data_path = data_path
-        self.__ground_truth_path = ground_truth_path
+        self._data_path = os.path.join(data_path, "kitti_stixel_images")
+        self._ground_truth_path = ground_truth_path
 
         # each line in ground truth contains the following information
         # series_date series_id frame_id x y point_type(Train/Test)
         # Eg: 09_26 1 55 242 Train
-        lines = [line.rstrip("\n") for line in open(self.__ground_truth_path, "r")]
+        lines = [line.rstrip("\n") for line in open(self._ground_truth_path, "r")]
         assert len(lines) > 0
         lines = [line.split("\t") for line in lines]
 
-        self.__lines = [
+        assert phase in ("train", "val")
+        phase_dict = {"train": "Train", "val": "Test"}
+        self._lines = [
             {
                 "series_data": line[0],
                 "series_id": int(line[1]),
@@ -29,23 +31,23 @@ class KittiStixelDataset(object):
                 "y": int(line[4]),
                 "type": line[5],
             }
-            for line in lines
+            for line in lines if line[5] == phase_dict[phase]
         ]
         del lines
 
-        self.__image_dict = {}
-        for line in self.__lines:
-            cur_base_image_path = self.__generate_image_path_from_series_info(
+        self._image_dict = {}
+        for line in self._lines:
+            cur_base_image_path = self._generate_image_path_from_series_info(
                 line["series_data"], line["series_id"], line["frame_id"]
             )
-            if cur_base_image_path in self.__image_dict.keys():
-                self.__image_dict[cur_base_image_path].append([line["x"], line["y"]])
+            if cur_base_image_path in self._image_dict.keys():
+                self._image_dict[cur_base_image_path].append([line["x"], line["y"]])
             else:
-                self.__image_dict[cur_base_image_path] = []
-        self.__image_paths = list(self.__image_dict.keys())
-        self.__stixels_pos = list(self.__image_dict.values())
+                self._image_dict[cur_base_image_path] = []
+        self._image_paths = list(self._image_dict.keys())
+        self._stixels_pos = list(self._image_dict.values())
 
-    def __generate_image_path_from_series_info(self, series_data, series_id, frame_id):
+    def _generate_image_path_from_series_info(self, series_data, series_id, frame_id):
         base_image_name = "{:010d}.png".format(frame_id)
         base_image_dir = "2011_{}/2011_{}_drive_{:04d}_sync/image_02/data".format(series_data, series_data, series_id)
         base_image_path = os.path.join(base_image_dir, base_image_name)
@@ -53,14 +55,14 @@ class KittiStixelDataset(object):
         return base_image_path
 
     def __getitem__(self, idx):
-        base_image_path = self.__image_paths[idx]
-        abs_image_path = os.path.join(self.__data_path, base_image_path)
+        base_image_path = self._image_paths[idx]
+        abs_image_path = os.path.join(self._data_path, base_image_path)
         cur_image = cv2.imread(abs_image_path)
 
-        return cur_image, self.__stixels_pos[idx]
+        return cur_image, self._stixels_pos[idx]
 
     def __len__(self):
-        return len(self.__image_dict)
+        return len(self._image_dict)
 
     def visualize_stixel(self, image, stixel_pos, stixel_width=5, stixel_height=100, color=(0, 255, 0)):
         result = np.copy(image)
@@ -70,7 +72,7 @@ class KittiStixelDataset(object):
         return result
 
     def visualize_one_image(self, idx):
-        assert(idx < self.__len__())
+        assert idx < self.__len__()
 
         image, stixel_pos = self.__getitem__(idx)
 
---
+++ scripts/download_kitti_stixels.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import requests
+import tqdm
+
+
+_CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+
+def download_file_from_google_drive(id, destination):
+    URL = "https://docs.google.com/uc?export=download"
+
+    session = requests.Session()
+
+    response = session.get(URL, params={"id": id}, stream=True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = {"id": id, "confirm": token}
+        response = session.get(URL, params=params, stream=True)
+
+    save_response_content(response, destination)
+
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith("download_warning"):
+            return value
+
+    return None
+
+
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+    with open(destination, "wb") as f:
+        for chunk in tqdm.tqdm(response.iter_content(CHUNK_SIZE)):
+            if chunk:  # filter out keep-alive new chunks
+                f.write(chunk)
+
+
+def main():
+    data_path = os.path.join(_CURRENT_DIR, "../data")
+    file_id = "13heRc3iRHISjjsg1-ba2qk2fycznqFVc"
+    destination = os.path.join(data_path, "kitti_stixel_images.zip")
+    if not os.path.isfile(destination) and not os.path.isdir(os.path.join(data_path, "augmented_data")):
+        download_file_from_google_drive(file_id, destination)
+        os.system("cd {} && unzip kitti_stixel_images.zip -d .".format(data_path))
+
+
+if __name__ == "__main__":
+    main()
