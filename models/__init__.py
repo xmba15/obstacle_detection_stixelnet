db474c121efb10acec605d4814cf68e94ad0a8d6
xmba15 <thba1590@gmail.com>
2020-01-23 17:14:35 +0900

    build network

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from .stixel_loss import StixelLoss
-from .stixel_net import StixelNet
+from .stixel_net import build_stixel_net
--- models/stixel_net.py
+++ models/stixel_net.py
@@ -1,13 +1,99 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import tensorflow.keras as keras
+import tensorflow.keras.layers as layers
+import tensorflow.keras.models as models
 
 
-class StixelNet(keras.Model):
+def build_stixel_net(input_shape=(370, 800, 3)):
+    """
+    input_shape -> (height, width, channel)
+    """
+    img_input = keras.Input(shape=input_shape)
 
-    def __init__(self, input_size):
-        super(StixelNet, self).__init__(name="stixel net")
-        self._input_size = input_size
+    x = layers.Conv2D(64, (3, 3),
+                      activation='relu',
+                      padding='same',
+                      name='block1_conv1')(img_input)
+    x = layers.Conv2D(64, (3, 3),
+                      activation='relu',
+                      padding='same',
+                      name='block1_conv2')(x)
+    x = layers.MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
 
-    def call(self, inputs):
-        pass
+    # Block 2
+    x = layers.Conv2D(128, (3, 3),
+                      activation='relu',
+                      padding='same',
+                      name='block2_conv1')(x)
+    x = layers.Conv2D(128, (3, 3),
+                      activation='relu',
+                      padding='same',
+                      name='block2_conv2')(x)
+    x = layers.MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
+
+    # Block 3
+    x = layers.Conv2D(256, (3, 3),
+                      activation='relu',
+                      padding='same',
+                      name='block3_conv1')(x)
+    x = layers.Conv2D(256, (3, 3),
+                      activation='relu',
+                      padding='same',
+                      name='block3_conv2')(x)
+    x = layers.Conv2D(256, (3, 3),
+                      activation='relu',
+                      padding='same',
+                      name='block3_conv3')(x)
+    x = layers.MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
+
+    # Block 4
+    x = layers.Conv2D(512, (3, 3),
+                      activation='relu',
+                      padding='same',
+                      name='block4_conv1')(x)
+    x = layers.Conv2D(512, (3, 3),
+                      activation='relu',
+                      padding='same',
+                      name='block4_conv2')(x)
+    x = layers.Conv2D(512, (3, 3),
+                      activation='relu',
+                      padding='same',
+                      name='block4_conv3')(x)
+
+    x = layers.Conv2D(256, (3, 3), strides=(1, 1), padding="same")(x)
+    x = layers.LeakyReLU()(x)
+    x = layers.Conv2D(256, (3, 3), strides=(1, 1), padding="same")(x)
+    x = layers.LeakyReLU()(x)
+    x = layers.MaxPooling2D((2, 1), strides=(2, 1))(x)
+
+    x = layers.Conv2D(256, (3, 3), strides=(1, 1), padding="same")(x)
+    x = layers.LeakyReLU()(x)
+    x = layers.Conv2D(256, (3, 3), strides=(1, 1), padding="same")(x)
+    x = layers.LeakyReLU()(x)
+    x = layers.MaxPooling2D((2, 1), strides=(2, 1), padding="same")(x)
+
+    x = layers.Conv2D(256, (3, 3), strides=(1, 1), padding="same")(x)
+    x = layers.LeakyReLU()(x)
+    x = layers.Conv2D(256, (3, 3), strides=(1, 1), padding="same")(x)
+    x = layers.LeakyReLU()(x)
+    x = layers.MaxPooling2D((2, 1), strides=(2, 1))(x)
+
+    x = layers.Conv2D(256, (3, 3), strides=(1, 1), padding="same")(x)
+    x = layers.LeakyReLU()(x)
+    x = layers.Conv2D(256, (3, 3), strides=(1, 1), padding="same")(x)
+    x = layers.LeakyReLU()(x)
+    x = layers.MaxPooling2D((2, 1), strides=(2, 1))(x)
+
+    x = layers.Conv2D(2048, (3, 1), strides=(1, 1), padding="valid")(x)
+    x = layers.LeakyReLU()(x)
+    x = layers.Conv2D(2048, (1, 3), strides=(1, 1), padding="same")(x)
+    x = layers.LeakyReLU()(x)
+    x = layers.Conv2D(2048, (1, 1), strides=(1, 1))(x)
+    x = layers.LeakyReLU()(x)
+
+    x = layers.Conv2D(50, (1, 1), strides=(1, 1), activation="softmax")(x)
+
+    model = models.Model(inputs=img_input, outputs=x)
+
+    return model
